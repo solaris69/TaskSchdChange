27f2dccd4a89f760af0e5189d37a6197ebb57636
solaris69 <vmanin@outlook.com>
2015-03-15 22:07:18 +0700

    Add missing main program

--- TaskSchdChange/Program.cs
+++ TaskSchdChange/Program.cs
@@ -1,43 +1,136 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Win32.TaskScheduler;
 using System.Security.Cryptography;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
 
 namespace TaskSchdChange
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+      static string schemaJson = @"{
+       '$schema': 'http://json-schema.org/draft-04/schema#',
+       'description': 'Puppet Task Scheduler resource set',
+       'type': 'array',
+       'items': {
+          'title' : 'Puppet Task Scheduler resource',
+          'type'  :  'object',
+          'properties' : {
+              'puppet_jobname': {
+                  'description': 'job name for puppet resource',
+                  'type':'string',
+                  'uniqueItems': true
+              },
+              'name': {
+                  'description': 'job name for puppet resource',
+                  'type':'string',
+                  'uniqueItems': true
+              },
+              'file': {
+                  'description': 'job name for puppet resource',
+                  'type':'string'
+              },
+              'interval': {
+                  'description': 'job name for puppet resource',
+                  'type':'number',
+                  'minimum': 1,
+                  'exclusiveMinimum': true
+              },
+              'enabled': {
+                  'description': 'job name for puppet resource',
+                  'type':'boolean'
+              }
+          },
+          'required': ['puppet_jobname','name','file','interval','enabled']
+        }
+      }";
+
+      static void Main(string[] args)
+      {
 #if DEBUG
           args = new[] { "test1", "5", "1" };
 //          args = new[] { "GoogleUpdateTaskMachineCore", "5", "1" };
 #endif
-          if (args.Length != 3) {
-            Console.WriteLine("Usage: [task scheduler name] [interval time to run task in MINUTES] [extend time to stop task in MINUTES]");
-            Environment.Exit(1);
-          }
-
+        var appSettings = ConfigurationManager.AppSettings;
+        if (Convert.ToBoolean(appSettings["switchPerTaskname"]))
+        {
           /*INSTALL via InstallShield -> setup /V "/v/qn INSTALLDIR=C:\_vc\tasks /L*V c:\_vc\vc.log"
            * Must be run as Administrator dos command box. setup_0.1.2.exe /S /V "/v/qn INSTALLDIR=C:\_vc\tasks"
            */
+          try
+          {
+            /*
+            using (JsonReader sreader = new JsonTextReader(schemaJson))
+            {
+            }
+            */
+            // http://www.newtonsoft.com/jsonschema/help/html/CreateJsonSchemaManually.htm
+            JSchema schema;
+            Type type = Type.GetType("TaskSchdOBJ");
+            Newtonsoft.Json.Schema.JsonSchemaGenerator jsonSchemaGenerator = new Newtonsoft.Json.Schema.JsonSchemaGenerator();
+            jsonSchemaGenerator.UndefinedSchemaIdHandling = Newtonsoft.Json.Schema.UndefinedSchemaIdHandling.UseTypeName;
+            Newtonsoft.Json.Schema.JsonSchema jsonSchema = jsonSchemaGenerator.Generate(type);
+            using (StreamWriter wr = new StreamWriter(@"T:\_GitCodingSchemaA.json"))
+            {
+              using (JsonWriter writer = new JsonTextWriter(wr))
+              {
+                writer.Formatting = Formatting.Indented;
+
+                jsonSchema.WriteTo(writer);
+              }
+            }
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine(ex.Message);
+          }
 
           using (var md5 = MD5.Create())
           {
+            // Get config file
+
+            if (!(File.Exists(appSettings["schdFileConfig"])))
+            {
+              Environment.Exit(2);
+            }
             /* USE MD5 check diff file
              * - tasklist.conf contains data as puppet create
-             * - tasklist.conf.md5 is check by this.
-            using (var stream = File.OpenRead(filename))
+             * - tasklist.conf.md5 is check by this.*/
+            bool diff = false;
+            using (var stream = File.OpenRead(appSettings["schdFileConfig"]))
             {
-              return md5.ComputeHash(stream);
+              byte[] diff_file = md5.ComputeHash(stream);
             }
-             */ 
-          }
+            if (diff)
+            {
+              using (StreamReader sr = new StreamReader(appSettings["schdFileConfig"]))
+              {
 
-          using (TaskService tsMgr = new TaskService() )
+              }
+
+            }
+          }
+          using (TaskService tsMgr = new TaskService())
+          {
+            //Usage: [task scheduler name] [interval time to run task in MINUTES] [extend time to stop task in MINUTES]
+            Task ts = tsMgr.GetTask(args[0]);
+            //if (ts != null)
+          }
+        }
+        else
+        {
+          if (args.Length != 3)
+          {
+            Console.WriteLine("Usage: [task scheduler name] [interval time to run task in MINUTES] [extend time to stop task in MINUTES]");
+            Environment.Exit(1);
+          }
+          using (TaskService tsMgr = new TaskService())
           {
             Task ts = tsMgr.GetTask(args[0]);
             if (ts != null)
@@ -60,5 +153,6 @@ namespace TaskSchdChange
             }
           }
         }
+      }
     }
 }
