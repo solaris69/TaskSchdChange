92bb2c07842c6fb8019efc31fcbf035c366c0619
solaris69 <vmanin@outlook.com>
2015-04-06 19:34:18 +0700

    new class to support task scheduler object for managing via puppet

---
+++ TaskSchdChange/TaskSchdOBJ.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSchdChange
+{
+  public class TaskSchdOBJ
+  {
+    public string puppet_jobname;
+    public string name;
+    public string file;
+    public int interval;
+    public bool enabled;
+  }
+}
